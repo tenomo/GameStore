e22dbc97d4c62ad1dd2988dc275f521c4c9e4118
Root <Root>
2016-07-06 03:15:03 +0300

    worked view list from db

---
+++ GameStore.WUI/Controllers/TwoGameController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameStore.WUI.Controllers
+{
+    public class TwoGameController : Controller
+    {
+        GameStore.WUI.Models.GameStoreDataBaseEntities context = new Models.GameStoreDataBaseEntities();
+        // GET: TwoGame
+        public ActionResult Index()
+        {
+            List<GameStore.WUI.Models.Game> games = new
+                List<Models.Game>(context.Game);
+            return View(games);
+        }
+    }
+}
